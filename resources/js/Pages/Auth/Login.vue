531c84bcd84d961727216dd78285cb44bbde4347
harold <haroldpeter6@gmail.com>
2026-02-25 23:05:48 +0300

    fix google auth

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -97,7 +97,7 @@ const submit = () => {
                 </div>
             </div>
 
-            <Link
+            <a
                 :href="route('auth.redirect', 'google')"
                 class="mt-4 w-full flex items-center justify-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 transition"
             >
@@ -108,7 +108,7 @@ const submit = () => {
                     <path fill="#EA4335" d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"/>
                 </svg>
                 <span class="ms-2">Google</span>
-            </Link>
+            </a>
         </div>
     </AuthenticationCard>
 </template>
